2ac6a74e2b6d1283bb48c17152314124563bd993
Tushita Wadhawan <twadhawan@berkeley.edu>
2025-01-31 18:25:28 -0800

    Adding homework1.py

---
+++ homework1.py
@@ -0,0 +1,23 @@
+#Homework #1
+#Problem 1: 
+#pwd: print working directory; tells you the path of your current directory in your computer; you would use it to determine which directory you are currently in 
+#cd: changes directory to the directory followed by cd; use it to go to a new directory
+#ls: list; lists all the files and folders within a directory; use it to see the list of contents within a directory or folder; -a flag shows all files, including hidden ones
+#mkdir: make directory; use to make a new directory/folder
+# cp: copy; can be used to copy files, (-r copies a directory)
+# mv: rename a file; use to rename a file, enter mv, followed by the name of the file we want to rename, and then its new name
+
+# Problem 2
+# Answer the following questions:
+# 1. What is the difference between Git, GitHub and Git Bash?
+#Git Bash is a command like interface which acts as a terminal to run Git commands. It allows the user to interact with repositories in Github, push change, rename files etc. GitHub is a platform that allows people to collaborate, share code, or work on projects simulataneously amongst other things. Git is a version control system that stores all the files and folders and their version histories.
+# 2. What is the difference between the terminal and the command line?
+# the terminal is the window where you execute commands. A command line is a specific line on the terminal where you input the command. 
+# 3. What is the difference between the Windows PowerShell and Git Bash?
+# They all use a different coding language and syntax. 
+# 4. What is the difference between Anaconda, conda and Python?
+# Conda is the package manager. Anaconda is a python distribution that includes a set of hundred packages including conda, jupyter lab etc. Python is a programming language. 
+# 5. What is a Jupyter Notebook? What is Jupyter Lab?
+# Jupyter notebook is a web-based interactive environment where you can create and share documents containing code, text, equations, and visualizations. Jupyter lab is more advanced and allows you to work on multiple projects at once.
+# 6. What is VS Code?
+# VS code allows you to edit code like .py files without using jupyter notebook.
